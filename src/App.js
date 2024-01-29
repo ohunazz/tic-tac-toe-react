a89c5e01b5f08d9329829c7caa5f8fde67219694
ohunazz <ohunazz@gmail.com>
2024-01-28 20:51:23 -0500

    Finish Tic-Tac-Toe

--- src/App.js
+++ src/App.js
@@ -1,9 +1,78 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Square from "./Components/Square";
+import { Patterns } from "./Patterns";
 
 function App() {
     const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+    const [player, setPlayer] = useState("O");
+    const [result, setResult] = useState({ winner: "none", state: "none" });
+
+    useEffect(() => {
+        checkWin();
+        checkIfTie();
+
+        if (player == "X") {
+            setPlayer("O");
+        } else {
+            setPlayer("X");
+        }
+    }, [board]);
+
+    useEffect(() => {
+        if (result.state != "none") {
+            alert(`Game Finished! Winning Player: ${result.winner}`);
+            restartGame();
+        }
+    }, [result]);
+
+    const chooseSquare = (square) => {
+        setBoard(
+            board.map((val, idx) => {
+                if (idx == square && val == "") {
+                    return player;
+                }
+
+                return val;
+            })
+        );
+    };
+
+    const checkWin = () => {
+        Patterns.forEach((currPattern) => {
+            const firstPlayer = board[currPattern[0]];
+            if (firstPlayer == "") return;
+            let foundWinningPattern = true;
+            currPattern.forEach((idx) => {
+                if (board[idx] != firstPlayer) {
+                    foundWinningPattern = false;
+                }
+            });
+
+            if (foundWinningPattern) {
+                setResult({ winner: player, state: "Won" });
+            }
+        });
+    };
+
+    const checkIfTie = () => {
+        let filled = true;
+        board.forEach((square) => {
+            if (square == "") {
+                filled = false;
+            }
+        });
+
+        if (filled) {
+            setResult({ winner: "No One", state: "Tie" });
+        }
+    };
+
+    const restartGame = () => {
+        setBoard(["", "", "", "", "", "", "", "", ""]);
+        setPlayer("O");
+    };
+
     return (
         <div className="App">
             <div className="board">
@@ -27,8 +96,46 @@ function App() {
                         }}
                     />
                 </div>
-                <div className="row"></div>
-                <div className="row"></div>
+                <div className="row">
+                    <Square
+                        val={board[3]}
+                        chooseSquare={() => {
+                            chooseSquare(3);
+                        }}
+                    />
+                    <Square
+                        val={board[4]}
+                        chooseSquare={() => {
+                            chooseSquare(4);
+                        }}
+                    />
+                    <Square
+                        val={board[5]}
+                        chooseSquare={() => {
+                            chooseSquare(5);
+                        }}
+                    />
+                </div>
+                <div className="row">
+                    <Square
+                        val={board[6]}
+                        chooseSquare={() => {
+                            chooseSquare(6);
+                        }}
+                    />
+                    <Square
+                        val={board[7]}
+                        chooseSquare={() => {
+                            chooseSquare(7);
+                        }}
+                    />
+                    <Square
+                        val={board[8]}
+                        chooseSquare={() => {
+                            chooseSquare(8);
+                        }}
+                    />
+                </div>
             </div>
         </div>
     );
